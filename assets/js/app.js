ad2f547369d3158a178b818e5eea3a59f6154480
K Ruhl <ruhlk@hotmail.com>
2020-10-27 22:49:44 -0500

    work from this afternoon

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-   // if the SVG area isn't empty when the browser loads, remove it and replace it with a resized version of the chart
+   // if the SVG area isn't empty when the browser loads, remove it and replace it with a resized version of the chart (hendrance when doing bonus)
   let svgArea = d3.select("#scatter").select("svg");
 
    if (!svgArea.empty()) {
@@ -6,7 +6,7 @@
    }
  
 
-// set the size of the SVG
+// set the size of the SVG (in pixels)
 let svgWidth = 1500;
 let svgHeight = 1000;
 
@@ -14,7 +14,7 @@ let svgHeight = 1000;
 let margin = {
     top: 80,
     right: 80,
-    bottom: 80,
+    bottom: 100,
     left: 80
 };
 
@@ -23,7 +23,7 @@ let width = svgWidth - margin.left - margin.right;
 let height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group, and shif the group
-// wrapper creation
+// wrapper creation (drawlable area)
 let svg = d3.select('#scatter') // selects the html element you want to use by id
     .append('svg') //appending the svg
     .attr('width', svgWidth) // giving it an attribute of width
@@ -38,15 +38,15 @@ let scatterGroup = svg.append('g')
 d3.csv("assets/data/data.csv").then(function(healthData) {
     console.log(healthData)
 
-    //parse data, cast as numbers
+    //parse data, cast as numbers (data is an empty object, and we are going to add data to it)
     healthData.forEach(function(data) {
         data.age = +data.age;
         data.smokes = +data.smokes;
     });
 
-    // create scale funtions
+    // create scale funtions (what values will be on axis)
     let xLinearScale = d3.scaleLinear()
-        .domain([30, d3.max(healthData, d => d.age)])
+        .domain([30, d3.max(healthData, d => d.age)]) // the range of numbers on the axis
         .range([0,width]);
 
     let yLinearScale = d3.scaleLinear()
@@ -55,28 +55,37 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
 
 
     // create axis functions
-    let bottomAxis = d3.axisBottom(xLinearScale);
-    let leftAxis = d3.axisLeft(yLinearScale);
+    let xAxis = d3.axisBottom(xLinearScale);
+    let yAxis = d3.axisLeft(yLinearScale);
 
     // append the axis to the chart
     scatterGroup.append('g')
         .attr("transform", `translate(0, ${height})`)
-        .call(bottomAxis);
+        .call(xAxis);
 
     scatterGroup.append('g')
-        .call(leftAxis);
+        .call(yAxis);
 
     
     // create circles
-    let circlesGroup = scatterGroup.selectAll("circle")
+    let circlesGroup = scatterGroup.selectAll("circle") //selectAll because you want all to be circles
         .data(healthData)
         .enter()
-        .append("circle")
+
+    circlesGroup.append("circle")
         .attr("cx", d => xLinearScale(d.age))
         .attr("cy", d => yLinearScale(d.smokes))
         .attr("r", "15")
         .attr("fill", "blue")
         .attr("opacity", ".5");
+
+    // add state abbrev. to circles
+    circlesGroup.append("text")
+        .text(d => d.abbr)
+        .attr("x", d => xLinearScale(d.age))
+        .attr("y", d => yLinearScale(d.smokes))
+        .attr("color","red");
+        
 
     
     // intialize tool tip
@@ -112,7 +121,7 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
         .text("Number of Smokers");
 
     scatterGroup.append("text") // this will create the x axis label
-        .attr("transform", `translate(${width / 2}, ${height + margin.top +30})`)
+        .attr("transform", `translate(${width / 2}, ${height + margin.top +10})`)
         .attr("class", "axisText")
         .text("Age");
 }).catch(function(error) {
